e6e8b9549ca3d3c971b181b6116a3ab06625f5ab
Christy Lum <christy.lum121@gmail.com>
2020-07-29 03:27:18 +0000

    Cleaned up code for spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -10,39 +10,3 @@ const spinner2 = function(delay) {
 };
 
 spinner2(200);
-
-/* setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700); */
